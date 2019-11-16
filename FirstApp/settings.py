1deda9796f48d4ee4410e1c2e01661714906706d
jaden <jadenxxxl@gmail.com>
2019-11-16 15:41:40 +0100

    Fix bugs

--- FirstApp/settings.py
+++ FirstApp/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iq-3m$u=)-co4$p^=&sg8$6upkqhj369-2oge@(gh6@6@fz*37'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["firstdjangoherokuapp.herokuapp.com/"]
-
+ALLOWED_HOSTS = ["127.0.0.1", "127.0.0.1:8000"]
 
 # Application definition
 
@@ -121,5 +120,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'

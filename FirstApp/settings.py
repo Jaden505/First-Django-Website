171a92c08d436eae5954333add09b8f9f98f8a6f
jaden <jadenxxxl@gmail.com>
2019-11-28 21:39:55 +0100

    postgreSQL added

--- FirstApp/settings.py
+++ FirstApp/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'FirstApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PostgreSQL 12',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
